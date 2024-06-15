85bdd7119d31fccf08d035cd8745b131701e2265
Sorabhss <salokhesourabh09@gmail.com>
2024-06-15 14:32:21 +0530

    changes

--- job.groovy
+++ job.groovy
@@ -6,7 +6,6 @@ folder('GCP') {
                     git {
                         remote {
                             url('https://github.com/Sorabhss/terraform-gcp-vm.git')
-                            credentials('gcpcred')
                             branch('main')
                         }
                     }
