be4dac9f2bd15ed3bdd0316401435622fc7844f3
Sourabh Salokhe <101861212+Sorabhss@users.noreply.github.com>
2024-06-15 11:27:28 +0530

    Update job.groovy

--- job.groovy
+++ job.groovy
@@ -1,4 +1,6 @@
-pipelineJob('Dev Pipeline Job') {
+import org.jenkinsci.plugins.workflow.cps.global.NonCPS
+
+pipelineJob('TerraformTesting') {
     definition {
         cpsScm {
             scm {
@@ -14,34 +16,65 @@ pipelineJob('Dev Pipeline Job') {
     }
 }
 
-pipelineJob('Release Job') {
-    definition {
-        cpsScm {
-            scm {
-                git {
-                    remote {
-                        url('https://github.com/Sorabhss/terraform-gcp-vm.git')
-                        branch('main')
+@NonCPS
+def createJobs() {
+    // Define the folder name
+    def folderName = 'Your Application Name'
+
+    // Create the folder
+    folder(folderName) {
+        // Define Dev Pipeline Job
+        pipelineJob('Dev Pipeline Job') {
+            definition {
+                cpsScm {
+                    scm {
+                        git {
+                            remote {
+                                url('https://github.com/Sorabhss/terraform-gcp-vm.git')
+                                branch('main')
+                            }
+                        }
                     }
+                    scriptPath('Jenkinsfile')
                 }
             }
-            scriptPath('release.Jenkinsfile')
+        }
+
+        // Define Release Job
+        pipelineJob('Release Job') {
+            definition {
+                cpsScm {
+                    scm {
+                        git {
+                            remote {
+                                url('https://github.com/Sorabhss/terraform-gcp-vm.git')
+                                branch('main')
+                            }
+                        }
+                    }
+                    scriptPath('release.Jenkinsfile')
+                }
+            }
+        }
+
+        // Define Master Pipeline Job
+        pipelineJob('Master Pipeline Job') {
+            definition {
+                cpsScm {
+                    scm {
+                        git {
+                            remote {
+                                url('https://github.com/Sorabhss/terraform-gcp-vm.git')
+                                branch('master')
+                            }
+                        }
+                    }
+                    scriptPath('master.Jenkinsfile')
+                }
+            }
         }
     }
 }
 
-pipelineJob('Master Pipeline Job') {
-    definition {
-        cpsScm {
-            scm {
-                git {
-                    remote {
-                        url('https://github.com/Sorabhss/terraform-gcp-vm.git')
-                        branch('master')
-                    }
-                }
-            }
-            scriptPath('master.Jenkinsfile')
-        }
-    }
-}
+// Call the method to create the jobs
+createJobs()
