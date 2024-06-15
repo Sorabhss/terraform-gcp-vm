e5af299edc64b0cee1444be0d117f983accbdc42
Sourabh Salokhe <101861212+Sorabhss@users.noreply.github.com>
2024-06-15 10:44:14 +0530

    Job

---
+++ job.groovy
@@ -0,0 +1,47 @@
+pipelineJob('Dev Pipeline Job') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('https://github.com/Sorabhss/terraform-gcp-vm.git')
+                        branch('main')
+                    }
+                }
+            }
+            scriptPath('Jenkinsfile')
+        }
+    }
+}
+
+pipelineJob('Release Job') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('https://github.com/Sorabhss/terraform-gcp-vm.git')
+                        branch('main')
+                    }
+                }
+            }
+            scriptPath('release.Jenkinsfile')
+        }
+    }
+}
+
+pipelineJob('Master Pipeline Job') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('https://github.com/Sorabhss/terraform-gcp-vm.git')
+                        branch('master')
+                    }
+                }
+            }
+            scriptPath('master.Jenkinsfile')
+        }
+    }
+}
